d67ca94b34e0dadbecac1dc20b6001e4ff23d43a
pallavicrm <88662369+pallavicrm@users.noreply.github.com>
2021-08-10 16:01:54 -0400

    new ipython note book added

---
+++ Sum of two numbers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def sum(a,b):
+ return a+b
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:"))
+print("sum is:", sum(n1,n2))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
